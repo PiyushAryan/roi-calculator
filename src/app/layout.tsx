8617bb7cc031e1d164cc4cfee686a32f14673c1c
Piyush Aryan <PiyushAryan81@gmail.com>
2025-07-24 07:31:54 +0000

    use the Sora font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className="antialiased">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="antialiased font-body">
         {children}
         <Toaster />
       </body>
